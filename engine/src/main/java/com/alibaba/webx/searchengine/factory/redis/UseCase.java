9503d8a6492d9ea01513eec549cd166a2ddfcd89
xiaoMzjm <383248369@qq.com>
2016-03-29 23:05:23 +0800

    完善redis的使用用例

--- engine/src/main/java/com/alibaba/webx/searchengine/factory/redis/UseCase.java
+++ engine/src/main/java/com/alibaba/webx/searchengine/factory/redis/UseCase.java
@@ -5,8 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import redis.clients.jedis.Jedis;
 
+import com.alibaba.webx.common.po.demo.Demo;
+
 /**
  * 【redis组件 使用用例】
+ * 
+ * API	:	http://tool.oschina.net/apidocs/apidoc?api=jedis-2.1.0
+ * 
+ * 入门	：	http://www.tuicool.com/articles/vaqABb
  * 
  * @author xiaoMzjm
  */
@@ -21,13 +27,22 @@ public class UseCase {
 		try {
 			jedis = redisFactory.getJedis();
 			
-			// 操作jedis
-			// ...
-			// ...
+			// 普通的操作直接看官网API，太多了，下面演示下如何存储java对象，利用到本包下的序列化工具类
+
+			Demo demo = new Demo();
+			
+			// 存储java对象
+			jedis.set("key".getBytes(), SerializeUtil.serialize(demo));
+			
+			// 获取java对象
+			Demo d = (Demo) SerializeUtil.unserialize(jedis.get("key".getBytes()));
+			
+			System.out.println(d);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
+			// 记得关闭喔！
 			jedis.close();
 		}
 	}
